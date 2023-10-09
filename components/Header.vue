920872b8f437dee09e04734590826147a65b7a66
vladdizlaw <vladdizlaw@gmail.com>
2023-10-09 21:46:53 +0300

    new design

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,28 @@
 <script setup>
+import { ref,onMounted } from 'vue'
 const emit = defineEmits(['card-select'])
+const cardIndex = ref(null)
+const cardValues=['Услуги','Логистика','Наши возможности','Наши клиенты']
 const handleClick = (linkName) => {
+  cardIndex.value = linkName
   emit('card-select', linkName)
 }
+onMounted ( () => {
+  // emit('card-select', cardIndex.value)
+})
 </script>
 <template>
   <div
     class="w-full flex flex-col items-center justify-center flex-row gap-2 p-[1rem] md:h-[max(23vh,8rem)] h-fit relative    header select-none shrink-0 z-10 relative">
 
-    <div class="w-full flex items-center justify-start flex-row gap-6  ">
+    <div class="w-full flex items-center justify-start flex-row gap-6  overflow-hidden">
       <div class="max-h-[80%] md:h-full  z-[10] w-[max(4vw,8rem)] md:w-[14vw] title flex md:ml-[2rem]">
         <img src="~assets/logo.svg" alt="">
-
       </div>
       <div class=" w-fit place-self-start place-items-center md:mt-6">
 
         <h1 class="title  text-[#ce8421] ">
+        
           ЧЕРНОМОРСКАЯ СТИВИДОРНАЯ КОМПАНИЯ
         </h1>
         <h2 class="title_sm text-xl text-[#d6b659] drop-shadow-xl">
@@ -23,12 +30,9 @@ const handleClick = (linkName) => {
         </h2>
       </div>
     </div>
-    <div class="w-full  md:justify-center md:gap-20  justify-around sm:gap-6 text-[#ce8421] flex absolute md:bottom-2 bottom-[-2rem] z-[100]">
-      <p @click="handleClick(0)" class="cursor-pointer title_sm_sm">Услуги</p>
-      <p @click="handleClick(1)" class="cursor-pointer title_sm_sm">Логистика</p>
-      <p @click="handleClick(2)" class="cursor-pointer title_sm_sm">Наши возможности</p>
-      <p @click="handleClick(3)" class="cursor-pointer title_sm_sm">Наши клиенты</p>
-      <p></p>
+    <div
+      class="w-full  md:justify-center md:gap-20  justify-around sm:gap-6 text-[#ce8421] flex absolute md:bottom-2 bottom-[-2rem] z-[100]">
+      <p v-for="card,index in cardValues" :key="index"  @click="handleClick(index)" class="cursor-pointer title_sm_sm " :class="{ 'active': cardIndex == index }">{{card}}</p>
     </div>
   </div>
 </template>
@@ -44,8 +48,13 @@ const handleClick = (linkName) => {
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   backdrop-filter: blur(8.5px);
   -webkit-backdrop-filter: blur(8.5px);
-  border-radius:0 0 10px 10px;
+  border-radius: 0 0 10px 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
+}
+
+.active {
+  color:rgb(248, 211, 29);
+  filter: drop-shadow(-10px 6px 4px #000);
 }
 
 .title {
